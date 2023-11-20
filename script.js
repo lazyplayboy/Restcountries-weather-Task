6dc54bdf6c1ebf10db20ecaa21cfd2a0104d2182
lazyplayboy <119006201+lazyplayboy@users.noreply.github.com>
2023-11-20 15:28:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,82 +1,50 @@
-async function restdata()
-{
+const container = document.createElement("div");
+container.className = "container";
+document.body.append(container);
 
-let data=await fetch("https://restcountries.com/v2/all");
-let data1= await data.json();
-console.log(data1);
-var container = document.createElement("div");
-container.setAttribute("class","container");
+async function getCountries() {
+  try {
+    let restCountries = await fetch("https://restcountries.com/v3.1/all");
+    let countryData = await restCountries.json();
+    console.log(countryData);
 
-var row = document.createElement("div");
-row.setAttribute("class","row");
-try
-{
-     
-        for(var i=0;i<data1.length;i++)
-        {
-            var name = data1[i].name;
-            var countryname = name.split(" ").join("");
-            
-            
-            var lat = data1[i].latlng && data1[i].latlng[0];
-            var lon = data1[i].latlng && data1[i].latlng[1];
+    countryData.forEach((country) => {
+      const card = document.createElement("div");
+      card.className = "card"
+      card.className = "mt-5"
+      card.style = "width: 18rem; text-align: center; border:1px solid gray;";
 
-            var btnclk = {name:countryname,lat:lat,lon:lon};
-            btnclk = JSON.stringify(btnclk);
-            
-            var col=document.createElement("div");
-            col.setAttribute("class","col-lg-4 col-sm-12");
-            col.innerHTML = `<div class="smallbox">
-                                <div class="card" style="width: 22rem;">
-                                        <div class="card-header">
-                                        ${name}
-                                        </div>
-                                    <img src="${data1[i].flag}" class="card-img-top" alt="...">
-                                    <div class="card-body">
-                                    <p class="card-text">Capital : ${data1[i].capital}</p>
-                                    <p class="card-text">Region : ${data1[i].region}</p>
-                                    <p class="card-text">Country code : ${data1[i].alpha3Code}</p>
-                                    <p class="card-text">Latitude : ${lat}</p>
-                                    <p class="card-text">longitude : ${lon}</p>
-                                    <button class="btn btn-primary" type="button" id="btn${i}" value=${btnclk} >Click for Whether</button>
-                                    
-                                    
-                                    
-                                    
-                                    
-                                </div>
-                            </div>`;
-                            
-            row.append(col);
-            container.append(row);
-            document.body.append(container);
-            document.getElementById(`btn${i}`).addEventListener("click",(e)=>{
-                e.preventDefault();
-              var btn = e.target.value ;
-              btn = JSON.parse(btn);
-              opendata(btn.name,btn.lat,btn.lon);
-
-        });
-        }
+      card.innerHTML = `<h5 class="card-head">${country.name.common}</h5>
+                  <img src="${country.flags.png}" class="card-img-top" alt="...">
+                  <div class="card-body">
+                  <h6 class="card-title">Capital:  ${country.capital}</h6>
+                  <h6 class="card-title">Region:  ${country.region}</h6>
+                  <h6 class="card-title">Country Code:  ${country.borders}</h6>
+                  <button onclick="getWeather('${country.name.common}')" class="btn btn-warning ">Click for Weather</button>
+                  </div>
+  `;
+      container.appendChild(card);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
-catch (error) {
-    console.log("invalid "+error.message);
-}
-}
-restdata();
-async function opendata(name,lat,lon)
-{
-    // console.log("opendata");
-    try {
-    let res=await fetch(`
-    https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=15e4f3b5c1edde11fbf448aae0e5e8a2`);
-    let res1= await res.json();
+
+getCountries();
 
 
-    alert(`Country name: ${name} , 
-Temp: ${res1.main.temp}`);
-    // alert(`Country name:${name}`);
-    } catch (error) {
-        console.log(error.message);
-    }
+async function getWeather(countryName) {
+  try {
+       let fetchWeather = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${countryName}&appid=cbaac264b1a3bdf7099d6b56f3d31bd3`
+    );
+       let dataWeather = fetchWeather.json();
+          console.log(dataWeather);
+
+       dataWeather.then(function (data) {
+          alert(`The ${countryName} Temperature is : ${data.main.temp}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
